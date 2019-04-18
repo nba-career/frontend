a19f2c0a892607283c3dd699fa0fd4265df91066
Seth Lachman <lachmanseth17@gmail.com>
2019-04-18 18:50:37 -0400

    Added styling to select file don team page.

--- nba/src/components/TeamPage.js
+++ nba/src/components/TeamPage.js
@@ -80,7 +80,7 @@ const styles = theme => ({
   },
 
   mainFeaturedPost: {
-    backgroundColor: theme.palette.grey[800],
+    backgroundColor: theme.palette.grey[300],
     color: theme.palette.common.white,
     marginBottom: theme.spacing.unit * 4,
     minHeight: "500px",
@@ -102,17 +102,35 @@ const styles = theme => ({
     width: 160
   },
   dropdown: {
-    height: 20
+    display: "block",
+    fontSize: 18,
+    fontWeight: 700,
+    color: "#444",
+    lineHeight: 1.3,
+    padding: ".6em 1.4em .5em .8em",
+    width: "100%",
+    maxWidth: "100%",
+    boxSizing: "border-box",
+    margin: "20px 60px",
+    border: "1px solid #838383",
+    boxShadow: "0px 4px 14px -3px rgba(0,0,0,.4)",
+    borderRadius: "1em",
+    appearance: "none",
+    backgroundColor: "#fff",
+    backgroundImage: "url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'), linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%)",
+    backgroundRepeat: "no-repeat, repeat",
+    backgroundPosition: "right .7em top 50%, 0 0",
+    backgroundSize: ".65em auto, 100%"
   },
   table: {
     minWidth: 700
   },
   tableCell: {
-    color: "white",
+    color: "black",
     fontSize: 16
   },
   player: {
-    color: "white",
+    color: "black",
     textDecoration: "none",
     "&:hover": {
       textDecoration: "underline"
